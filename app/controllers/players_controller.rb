bdf83cb93243f73ec2535dbe4e5c327881cb4630
trittycoding91 <trittycoding91@icloud.com>
2020-10-19 12:06:38 -0500

    Added pagination using will_paginate gem and will_paginate-bulma gem, pagination fully working for players

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,9 +1,11 @@
 class PlayersController < ApplicationController
+
   def index
-    @players = Player.all
+    @players = Player.paginate(:page => params[:page], :per_page => 50)
   end
 
   def show
     @player = Player.find(params[:id])
   end
+
 end
