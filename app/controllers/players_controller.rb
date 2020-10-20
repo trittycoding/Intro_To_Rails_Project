e371a107486470f65bad8350b13505b87d0cbff8
trittycoding91 <trittycoding91@icloud.com>
2020-10-20 16:24:27 -0500

    Added search functionality to players view

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -8,4 +8,12 @@ class PlayersController < ApplicationController
     @player = Player.find(params[:id])
   end
 
+  # Searches for players based on keywords returned by the form
+  def search
+    # Recall that '%' represents wildcard in SQL
+    search_by_keywords = "%#{params[:keywords]}%"
+
+    # @returned_players will be shared with /players/search.html.erb
+    @returned_players = Player.where("name LIKE ?", search_by_keywords).paginate(:page => params[:page], :per_page =>  50)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-resources :players, only: [:index, :show]
+resources :players, only: [:index, :show] do
+  # Allows for searching through players#search --> search_players_path
+  collection do
+    get "search"
+  end
+end
 resources :teams, only: [:index, :show]
 resources :roles, only: [:index, :show]
 root to: 'home#index'
